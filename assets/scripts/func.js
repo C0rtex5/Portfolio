17570fd310d8957393022d8beef488860e3f28df
Abhay <4bh4y.m3nd3s@gmail.com>
2023-12-04 17:02:24 +0000

    update

---
+++ assets/scripts/func.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const textToType = "Human";
+  const textElement = document.getElementById("typing-text");
+  let displayedText = "";
+  let index = 0;
+
+  function typeText() {
+    if (index < textToType.length) {
+      displayedText += textToType.charAt(index);
+      textElement.textContent = displayedText;
+      index++;
+      setTimeout(typeText, 150); // Typing speed (adjust as needed)
+    }
+  }
+
+  typeText();
+});
